a814feb1d76d04f447f3455c6fc1243e7ac9501a
pranav tengshe <pranavtengshe@gmail.com>
2020-06-26 08:34:13 +0000

    my commit2

--- data_prep.R
+++ data_prep.R
@@ -1,11 +1,18 @@
 library(dplyr)
 
-County_Pop = read.csv('Data/co-est2019-alldata.csv', stringsAsFactors = F)
+County_Pop = read.csv('co-est2019-alldata.csv', stringsAsFactors = F)
+
+County_Covid_Counts= read.csv('https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv', stringsAsFactors = F)
 
 glimpse(County_Pop)
 
-County_Pop['CountyMIPS']= paste('0',County_Pop['CountyMIPS'])
-
 County_Pop= County_Pop[, c('CountyMIPS','STATE','STNAME','POPESTIMATE2019','RNETMIG2019')]
 
-County_Pop
+County_Covid_Counts_agg= County_Covid_Counts %>% 
+  group_by(fips) %>% 
+  mutate(totalcases= sum(cases), totaldeaths= sum(deaths)) %>%
+  arrange(date) %>% 
+  mutate( rrate= mean(cases/lag(cases), na.rm=T )) %>% 
+  select (fips, totalcases,totaldeaths,rrate)
+
+View(County_Covid_Counts_agg)
